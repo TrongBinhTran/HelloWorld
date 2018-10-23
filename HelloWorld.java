1b6a4f346a15394a5aef1927af705395dc6fadba
Binh Tran <BinhTT13@fsoft.com.vn>
2018-10-23 11:17:17 +0700

    Modifed Main Class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
 
     public static void main(String[] args) {
-        System.out.println("Hello World Binh handsome !");
+        System.out.println("Hello Binh handsome !");
     }
 
 }
